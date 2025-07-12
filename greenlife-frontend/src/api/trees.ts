e5d5d028c5a5458f8fa426464596a27337ee52f4
Ubuntu <azureuser@devops-0.ka1k2qd4a05ulooziz1h5rfsjf.ax.internal.cloudapp.net>
2025-07-12 21:18:39 +0000

    frontend_update

--- greenlife-frontend/src/api/trees.ts
+++ greenlife-frontend/src/api/trees.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/trees/';
+const API_URL = 'http://134.149.216.180:8000/api/trees/';
 
 export const fetchTrees = () => axios.get(API_URL);
 export const fetchGeoJson = () => axios.get(`${API_URL}geojson/`);
